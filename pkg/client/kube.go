cdb41c9612c58037fcfa45b55e98221399ea48c0
Charles Moulliard <cmoulliard@redhat.com>
2018-07-05 10:35:15 +0200

    Refactor kube client

--- pkg/client/kube.go
+++ pkg/client/kube.go
@@ -8,10 +8,14 @@ import (
 	log "github.com/Sirupsen/logrus"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+	restclient "k8s.io/client-go/rest"
+
+	teamclientset "github.com/cmoulliard/k8s-team-crd/pkg/client/clientset/versioned"
 )
 
-// retrieve the Kubernetes cluster client from outside of the cluster
-func GetKubernetesClient() kubernetes.Interface {
+var config = parseKubernetesConfig()
+
+func parseKubernetesConfig() *restclient.Config {
 	var kubeconfig *string
 	if home := homeDir(); home != "" {
 		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
@@ -26,6 +30,26 @@ func GetKubernetesClient() kubernetes.Interface {
 	if err != nil {
 		log.Fatalf("getClusterConfig: %v", err)
 	}
+	return config
+}
+
+// Retrieve the Kubernetes cluster client from outside of the cluster and add the Team Clienset
+func GetKubernetesCRDClient() (kubernetes.Interface, teamclientset.Interface) {
+	// Generate the client based off of the config
+	client := GetKubernetesClient()
+
+	// Create a Team ClientSet
+	clientset, err := teamclientset.NewForConfig(config)
+	if err != nil {
+		log.Fatalf("Team clienset: %v", err)
+	}
+
+	log.Info("Successfully constructed k8s client")
+	return client, clientset
+}
+
+// Retrieve the Kubernetes cluster client from outside of the cluster
+func GetKubernetesClient() (kubernetes.Interface) {
 
 	// generate the client based off of the config
 	client, err := kubernetes.NewForConfig(config)
